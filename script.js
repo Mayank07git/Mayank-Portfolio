04cc810c393137ad1d8de09d3f1179c96e4fc08e
Mayank <ms6586864@gmail.com>
2024-03-18 12:06:21 +0530

    Code Updated

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".gradient", {
+    strings: ["Mayank Sharma.", "I'm a Frontend Developer", "and"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
