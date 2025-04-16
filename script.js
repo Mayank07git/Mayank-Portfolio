c1240eddc1dc962eba05d60ecf922d8e32819ecf
Mayank <ms6586864@gmail.com>
2025-04-16 13:49:03 +0530

    Code Updated

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ var typed = new Typed(".gradient", {
     backDelay: 1000,
     loop: true
 })
+
+// var typed = new Typed(".gradient", {
+//     strings: ["Mayank Sharma.", "I'm a Frontend Developer", "and"],
+//     typeSpeed: 50,
+//     backSpeed: 50,
+//     backDelay: 1000,
+//     loop: true,
+//     onStringTyped: function() {
+//         const fadeElement = document.querySelector('.gradient');
+//         fadeElement.style.opacity = 1;
+//         setTimeout(() => {
+//             fadeElement.style.opacity = 0;
+//         }, 500); 
+//     }
+// })
